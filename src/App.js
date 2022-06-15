ae9f5567635e106bd73fd30cfdd93bdd22659511
NwaguChima <nwaguchima48@gmail.com>
2022-06-15 20:29:10 +0100

    routing structure setup

--- src/App.js
+++ src/App.js
@@ -37,6 +37,35 @@ const App = () => {
               Navbar
             </div>
           </div>
+          <div>
+            <Routes>
+              {/* Dashboa */}
+              <Route path="/" element={<h1>Home</h1>} />
+              <Route path="/ecommerce" element={<h1>ECommerce</h1>} />
+
+              {/* Pages */}
+              <Route path="/orders" element={<h1>Orders</h1>} />
+              <Route path="/employees" element={<h1>Employees</h1>} />
+              <Route path="/customers" element={<h1>Customers</h1>} />
+
+              {/* Apps */}
+              <Route path="/kanban" element={<h1>Kanban</h1>} />
+              <Route path="/editor" element={<h1>Editor</h1>} />
+              <Route path="/calendar" element={<h1>Calendar</h1>} />
+              <Route path="/color-picker" element={<h1>Color Picker</h1>} />
+
+              {/* Charts */}
+              {/* routes for line, area, bar, pie, financial, color-mapping, pyramid, and stacked */}
+              <Route path="/line" element={<h1>Line</h1>} />
+              <Route path="/area" element={<h1>Area</h1>} />
+              <Route path="/bar" element={<h1>Bar</h1>} />
+              <Route path="/pie" element={<h1>Pie</h1>} />
+              <Route path="/financial" element={<h1>Financial</h1>} />
+              <Route path="/color-mapping" element={<h1>Color Mapping</h1>} />
+              <Route path="/pyramid" element={<h1>Pyramid</h1>} />
+              <Route path="/stacked" element={<h1>Stacked</h1>} />
+            </Routes>
+          </div>
         </div>
       </BrowserRouter>
     </div>
