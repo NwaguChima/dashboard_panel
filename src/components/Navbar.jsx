16fcf5e3ad96042354e636d67545f0ecb164d815
NwaguChima <nwaguchima48@gmail.com>
2022-06-17 13:42:58 +0100

    setup the click function for navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -32,8 +32,8 @@ const NavButton = ({ title, customFunc, icon, color, dotColor }) => {
 };
 
 const Navbar = () => {
-  const { activeMenu, setActiveMenu } = useStateContext();
-  const handleClick = () => {};
+  const { activeMenu, setActiveMenu, isClicked, setIsClicked, handleClick } =
+    useStateContext();
 
   return (
     <div className="flex justify-between p-2 md:mx-6 relative">
@@ -83,6 +83,10 @@ const Navbar = () => {
             <MdKeyboardArrowDown className="text-gray-400 text-14" />
           </div>
         </TooltipComponent>
+        {isClicked.cart && <Cart />}
+        {isClicked.chat && <Chat />}
+        {isClicked.notification && <Notification />}
+        {isClicked.userProfile && <UserProfile />}
       </div>
     </div>
   );
