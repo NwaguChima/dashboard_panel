b440dcf26181be287d778bead66f32f4d926ffca
NwaguChima <nwaguchima48@gmail.com>
2022-06-17 12:47:51 +0100

    completed sidebar display of sublinks

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -8,6 +8,11 @@ import { links } from "../data/dummy";
 
 const Sidebar = () => {
   const activeMenu = true;
+  const activeLink =
+    "flex items-center gap-5 pl-4 pt-3 pb-2.5 rounded-lg text-white text-md m-2";
+  const normalLink =
+    "flex items-center gap-5 pl-4 pt-3 pb-2.5 rounded-lg text-md text-gray-700 dark:text-gray-200 dark:hover:text-black hover:bg-light-gray m-2";
+
   return (
     <div className="ml-3 h-screen md:overflow-hidden overflow-auto md:hover:overflow-auto pb-10">
       {activeMenu && (
@@ -30,7 +35,26 @@ const Sidebar = () => {
               </button>
             </TooltipComponent>
           </div>
-          <div className="mt-10"></div>
+          <div className="mt-10">
+            {links.map((link) => (
+              <div key={link.title}>
+                <p className="text-gray-400 m-3 mt-4 uppercase">{link.title}</p>
+                {link.links.map((sublink) => (
+                  <NavLink
+                    to={`/${sublink.name}`}
+                    key={sublink.name}
+                    onclick={() => {}}
+                    className={({ isAcitve }) =>
+                      isAcitve ? activeLink : normalLink
+                    }
+                  >
+                    {sublink.icon}
+                    <span className="capitalize">{sublink.name}</span>
+                  </NavLink>
+                ))}
+              </div>
+            ))}
+          </div>
         </>
       )}
     </div>
