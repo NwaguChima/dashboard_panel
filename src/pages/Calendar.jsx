bff1a39da44a00798fd9228dc5783ab3c4901a54
NwaguChima <nwaguchima48@gmail.com>
2022-06-18 21:23:43 +0100

    rendered calender

--- src/pages/Calendar.jsx
+++ src/pages/Calendar.jsx
@@ -10,6 +10,7 @@ import {
   Inject,
   Resize,
   DragAndDrop,
+  WorkWeek,
 } from "@syncfusion/ej2-react-schedule";
 import { DatePickerComponent } from "@syncfusion/ej2-react-calendars";
 
@@ -20,6 +21,15 @@ const Calendar = () => {
   return (
     <div className="m-2 md:m-10 mt-24 p-2 md:p-10 bg-white rounded-3xl">
       <Header category="App" title="Calendar" />
+      <ScheduleComponent
+        height="650px"
+        eventSettings={{ dataSource: scheduleData }}
+      >
+        >
+        <Inject
+          services={[Day, Week, WorkWeek, Month, Agenda, Resize, DragAndDrop]}
+        />
+      </ScheduleComponent>
     </div>
   );
 };
