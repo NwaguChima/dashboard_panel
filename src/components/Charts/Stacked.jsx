70f6f5e9b8f573eda933a269535b5e17336e3d89
NwaguChima <nwaguchima48@gmail.com>
2022-06-18 08:58:24 +0100

    setup container for stacked component

--- src/components/Charts/Stacked.jsx
+++ src/components/Charts/Stacked.jsx
@@ -1,4 +1,19 @@
 import React from "react";
+import {
+  ChartComponent,
+  SeriesColectionDirective,
+  SeriesDirective,
+  Inject,
+  Legend,
+  Category,
+  StackingColumnSeries,
+  Tooltip,
+} from "@syncfusion/ej2-react-charts";
+import {
+  stackedPrimaryXAxis,
+  stackedPrimaryYAxis,
+  stackedCustomSeries,
+} from "../../data/dummy";
 
 const Stacked = () => {
   return <div>Stacked</div>;
