6c9af19bd75dd649ca47aef69df310e472e6979c
NwaguChima <nwaguchima48@gmail.com>
2022-06-18 21:08:36 +0100

    setup the component with imports

--- src/pages/Customers.jsx
+++ src/pages/Customers.jsx
@@ -1,7 +1,22 @@
 import React from "react";
+import {
+  GridComponent,
+  ColumnsDirective,
+  ColumnDirective,
+  Page,
+  Selection,
+  Inject,
+  Edit,
+  Toobar,
+  Sort,
+  Filter,
+} from "@syncfusion/ej2-react-grids";
+
+import { customersData, customersGrid } from "../data/dummy";
+import { Header } from "../components";
 
 const Customers = () => {
-  return <div>Customers</div>;
+  return <div></div>;
 };
 
 export default Customers;
