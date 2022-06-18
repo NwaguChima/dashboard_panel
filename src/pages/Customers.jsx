1179a8a153db2f165457799dea1b9dc3c873dcac
NwaguChima <nwaguchima48@gmail.com>
2022-06-18 21:13:05 +0100

    setup the customers table fully

--- src/pages/Customers.jsx
+++ src/pages/Customers.jsx
@@ -7,7 +7,7 @@ import {
   Selection,
   Inject,
   Edit,
-  Toobar,
+  Toolbar,
   Sort,
   Filter,
 } from "@syncfusion/ej2-react-grids";
@@ -16,7 +16,26 @@ import { customersData, customersGrid } from "../data/dummy";
 import { Header } from "../components";
 
 const Customers = () => {
-  return <div></div>;
+  return (
+    <div className="m-2 md:m-10 p-2 md:p-10 bg-white rounded-3xl">
+      <Header category="page" title="Customers" />
+      <GridComponent
+        dataSource={customersData}
+        allowPaging
+        allowSorting
+        toolbar={["Delete"]}
+        editSettings={{ allowEditing: true, allowDeleting: true }}
+        width="auto"
+      >
+        <ColumnsDirective>
+          {customersGrid.map((column, index) => (
+            <ColumnDirective key={index} {...column} />
+          ))}
+        </ColumnsDirective>
+        <Inject services={[Page, Toolbar, Selection, Edit, Sort, Filter]} />
+      </GridComponent>
+    </div>
+  );
 };
 
 export default Customers;
