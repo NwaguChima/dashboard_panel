35492df34710cb00026ec52cc24fd6230b30f635
NwaguChima <nwaguchima48@gmail.com>
2022-06-19 01:35:27 +0100

    line chart rendered

--- src/components/Charts/LineChart.jsx
+++ src/components/Charts/LineChart.jsx
@@ -1,7 +1,27 @@
 import React from "react";
+import {
+  ChartComponent,
+  SeriesCollectionDirective,
+  SeriesDirective,
+  Inject,
+  DateTime,
+  Tooltip,
+  LineSeries,
+  Legend,
+} from "@syncfusion/ej2-react-charts";
+
+import {
+  lineCustomSeries,
+  LinePrimaryYAxis,
+  LinePrimaryXAxis,
+} from "../../data/dummy";
 
 const LineChart = () => {
-  return <div>LineChart</div>;
+  return (
+    <ChartComponent>
+      <Inject services={[LineSeries, Tooltip, DateTime, Legend]} />
+    </ChartComponent>
+  );
 };
 
 export default LineChart;
